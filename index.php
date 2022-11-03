196fa6070494d272f92d31095583f8802a469c34
nahian28 <nahiansylhet@gmail.com>
2022-11-03 10:04:43 +0600

    Updates

--- index.php
+++ index.php
@@ -4,9 +4,156 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Ecommerce Website</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-3">
+                    <div class="logo">
+                        <a href="">ecommerce</a>
+                    </div>
+                </div>
+                <div class="col-xxl-6">
+                    <div class="menu">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Categorys</a></li>
+                            <li><a href="">Shop</a></li>
+                            <li><a href="">Cart</a></li>
+                            <li><a href="">Checkout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xxl-3">
+                    <div class="right">
+                    <a href="" class=" position-relative"><i class="fa-solid fa-cart-shopping"></i><span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+    12
+</a>
+                    <a href=""><i class="fa-solid fa-user"></i> Hi, User</a>
+                    <a href="" class="btn btn-primary">Login or Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="bannar" style="background-image:url('assets/img/bannar.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-12">
+                    <h1>Welcome to Our Shop</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<section class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-xxl-12">
+                <h4 class="section-title">Featured Categorys</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xxl-3">
+                <div class="card">
+                    <img src="assets/img/product1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3">
+                <div class="card">
+                    <img src="assets/img/product2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3">
+                <div class="card">
+                    <img src="assets/img/product3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3">
+                <div class="card">
+                    <img src="assets/img/product3.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <section class="cards">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-12">
+                    <h4 class="section-title">Featured Products</h4>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xxl-3">
+                    <div class="card">
+                        <img src="assets/img/product1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3">
+                    <div class="card">
+                        <img src="assets/img/product2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3">
+                    <div class="card">
+                        <img src="assets/img/product3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xxl-3">
+                    <div class="card">
+                        <img src="assets/img/product3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Add to Cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
